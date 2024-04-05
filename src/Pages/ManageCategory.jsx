561826aa2f735bc0f85c3291f13b2ba5f92ea706
Shruti-2303 <sharmas23032001@gmail.com>
2024-04-06 00:00:20 +0530

    Fix : vercel build fix

--- src/Pages/ManageCategory.jsx
+++ src/Pages/ManageCategory.jsx
@@ -1,17 +1,21 @@
-import Base from '../components/Base'
-import ManageCatComp from '../components/manageCatComp'
-import SideBar from '../components/sidebar/SideBar'
+import Base from "../components/Base";
+import ManageCatComp from "../components/ManageCatComp";
+import SideBar from "../components/sidebar/SideBar";
 
 const ManageCategory = () => {
   return (
     <Base>
-        <div style={{display:'flex'}}>
-            <div style={{flex:1}}><SideBar/></div>
-            
-            <div style={{flex:4}}><ManageCatComp/></div>
+      <div style={{ display: "flex" }}>
+        <div style={{ flex: 1 }}>
+          <SideBar />
         </div>
-    </Base>
-  )
-}
 
-export default ManageCategory
+        <div style={{ flex: 4 }}>
+          <ManageCatComp />
+        </div>
+      </div>
+    </Base>
+  );
+};
+
+export default ManageCategory;
